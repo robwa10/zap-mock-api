95beb189e513e3840d4a23e669416a8805883bce
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-12-28 06:26:43 +0000

    Parsing Headers

--- index.js
+++ index.js
@@ -23,11 +23,14 @@ const server = http.createServer((req, res) => {
   // Get the HTTP method
   const method = req.method.toUpperCase();
 
+  // Get the headers as an object
+  const headers = req.headers;
+
   // Send the response
   res.end('Hello World\n');
 
   // Log the request path
-  console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' with params', queryStringObject);
+  console.log('Request headers:', headers);
 
 });
 
