6b80dc7a245e94102336980c45874404adb224df
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-12-28 07:33:51 +0000

    Routing Requests

--- index.js
+++ index.js
@@ -37,14 +37,50 @@ const server = http.createServer((req, res) => {
 
     buffer += decoder.end();
 
-    // Send the response
-    res.end('Hello World\n');
+    // Choose the handler. Default to notFound
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-    // Log the request path
-    console.log('Request with this payload:', buffer);
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      'payload': buffer
+    };
 
+    // Route the request to correct handler
+    chosenHandler(data, (statusCode, payload) => {
+      // Use the handler status code or default to 200
+      statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+
+      // Use the handler payload or an empty object
+      payload = typeof(payload) === 'object' ? payload : {};
+
+      // Convert the payload to a string
+      const payloadString = JSON.stringify(payload)
+
+      // Return the response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Log the request path
+      console.log('Returning this response:', statusCode, payload);
+
+    });
   });
 });
 
 // Start the server on port 3000
 server.listen(3000, () => (console.log('Listening on port 3000.')));
+
+// Handlers
+const handlers = {
+  'sample': (data, callback) => (callback(200, {'name': 'sample handler'})),
+  'notFound': (data, callback) => (callback(404))
+};
+
+// Define a request router
+const router = {
+  'sample': handlers.sample
+};
