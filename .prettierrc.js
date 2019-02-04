58ccb0d84b8d86fd33e41ddf510afc6e8945fe44
Robert Hubbard <rob.c.hubbard@gmail.com>
2019-02-04 20:47:40 +0000

    Make all things pretty

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    printWidth: 80,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+};
